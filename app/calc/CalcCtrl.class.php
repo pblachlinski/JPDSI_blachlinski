9edc8c53b67b8cf4a145c63029c454b6ac00b635
Piotr-PC <Piotr-PC@Piotrek-PC>
2021-03-29 20:24:48 +0200

    Kalkulator z szablonowaniem

---
+++ app/calc/CalcCtrl.class.php
@@ -0,0 +1,134 @@
+<?php
+// W skrypcie definicji kontrolera nie trzeba dołączać problematycznego skryptu config.php,
+// ponieważ będzie on użyty w miejscach, gdzie config.php zostanie już wywołany.
+
+require_once $conf->root_path.'/lib/smarty/Smarty.class.php';
+require_once $conf->root_path.'/lib/Messages.class.php';
+require_once $conf->root_path.'/app/calc/CalcForm.class.php';
+require_once $conf->root_path.'/app/calc/CalcResult.class.php';
+
+/** Kontroler kalkulatora
+ * @author Przemysław Kudłacik
+ *
+ */
+class CalcCtrl {
+
+	private $msgs;   //wiadomości dla widoku
+	private $infos;  //informacje dla widoku
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+	private $hide_intro; //zmienna informująca o tym czy schować intro
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->msgs = new Messages();
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+		$this->hide_intro = false;
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->x = isset($_REQUEST ['x']) ? $_REQUEST ['x'] : null;
+		$this->form->y = isset($_REQUEST ['y']) ? $_REQUEST ['y'] : null;
+		$this->form->z = isset($_REQUEST ['z']) ? $_REQUEST ['z'] : null;
+	}
+	
+	/** 
+	 * Walidacja parametrów
+	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->x ) && isset ( $this->form->y ) && isset ( $this->form->op ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false;
+		} else { 
+			$this->hide_intro = true; //przyszły pola formularza - schowaj wstęp
+		}
+		
+		// sprawdzenie, czy potrzebne wartości zostały przekazane
+		if ($this->form->x == "") {
+			$this->msgs->addError('Nie podano pierwszej wartośći');
+		}
+		if ($this->form->y == "") {
+			$this->msgs->addError('Nie podano drugiej wartości');
+		}
+                if ($this->form->z == "") {
+			$this->msgs->addError('Nie podano trzeciej wartości');
+		}
+		
+		// nie ma sensu walidować dalej gdy brak parametrów
+		if (! $this->msgs->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if (! is_numeric ( $this->form->x )) {
+				$this->msgs->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			}
+			
+			if (! is_numeric ( $this->form->y )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			}
+                        
+                        if (! is_numeric ( $this->form->z )) {
+				$this->msgs->addError('Trzecia wartość nie jest liczbą całkowitą');
+			}
+		}
+		
+		return ! $this->msgs->isError();
+	}
+	
+	/** 
+	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->x = intval($this->form->x);
+			$this->form->y = intval($this->form->y);
+                        $this->form->z = intval($this->form->z);
+			$this->msgs->addInfo('Parametry poprawne.');
+				
+			//wykonanie operacji
+                        
+                        $this->result->result = $this->form->x * ((1 + ($this->form->z / 12)) ^ 12) * (((1 + ($this->form->z / 12)) - 1) / (((1 + ($this->form->z / 12)) ^ 12) - 1));
+                        
+                        
+			$this->msgs->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	public function generateView(){
+		global $conf;
+		
+		$smarty = new Smarty();
+		$smarty->assign('conf',$conf);
+		
+		$smarty->assign('page_title','Przykład 06');
+		$smarty->assign('page_description','Aplikacja z jednym "punktem wejścia". Model MVC, w którym jeden główny kontroler używa różnych obiektów kontrolerów w zależności od wybranej akcji - przekazanej parametrem.');
+		$smarty->assign('page_header','Kontroler główny');
+				
+		$smarty->assign('hide_intro',$this->hide_intro);
+		
+		$smarty->assign('msgs',$this->msgs);
+		$smarty->assign('form',$this->form);
+		$smarty->assign('res',$this->result);
+		
+		$smarty->display($conf->root_path.'/app/calc/CalcView.html');
+	}
+}
---
+++ templates_c/87815237ac1046fb6bb904bd4f9d411342c6ba01_0.file.CalcView.html.php
@@ -0,0 +1,137 @@
+<?php
+/* Smarty version 3.1.30, created on 2021-03-29 18:24:46
+  from "D:\Xampp\htdocs\php_06_kontroler_glowny\app\calc\CalcView.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_6061ff4e9416e7_25772803',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '87815237ac1046fb6bb904bd4f9d411342c6ba01' => 
+    array (
+      0 => 'D:\\Xampp\\htdocs\\php_06_kontroler_glowny\\app\\calc\\CalcView.html',
+      1 => 1617035085,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6061ff4e9416e7_25772803 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17895594116061ff4e92bc37_93513882', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_5503386856061ff4e9411c6_86414713', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender(($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.html"), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, true);
+}
+/* {block 'footer'} */
+class Block_17895594116061ff4e92bc37_93513882 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_5503386856061ff4e9411c6_86414713 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Kalkulator kredytowy</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute" method="post">
+		<fieldset>
+
+			<label for="x">Kwota kredytu</label>
+			<input id="x" type="text" placeholder="" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->x;?>
+">
+		
+			<label for="y">Ilośc lat</label>
+			<input id="y" type="text" placeholder="" name="y" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->y;?>
+">
+                        
+                        <label for="z">Oprocentowanie</label>
+			<input id="z" type="text" placeholder="" name="z" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->z;?>
+">
+
+			<button type="submit" class="pure-button">Oblicz</button>
+		</fieldset>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Wystąpiły błędy: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Informacje: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
